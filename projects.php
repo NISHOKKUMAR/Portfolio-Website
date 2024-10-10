3e278c38fa2da961b132553ee888aed82d5f9e40
NISHOKKUMAR <nishokkumar96@gmail.com>
2024-10-10 19:06:03 +0530

    minor correction on project page

--- projects.php
+++ projects.php
@@ -10,11 +10,11 @@ $result = $conn->query($query);
 ?>
 
 <div class="showcase">
-    <!-- Button which redirects to the page for adding a new project -->
+    <!-- Button which redirects to the page for adding a new project if your user-->
     <?php if (isset($_SESSION['logged']) && $_SESSION['logged']) : ?>
-        <div class="update-project">
-            <a href="UpdateProject.php">Add New Project</a>
-        </div>
+    <div class="update-project">
+        <a href="UpdateProject.php">Add New Project</a>
+    </div>
     <?php endif; ?>
 
 
@@ -25,11 +25,13 @@ $result = $conn->query($query);
             while ($row = $result->fetch_assoc()) {
                 echo '<div class="project">';
                     // Ensure the image URL is correctly formed
-                    echo '<div class="project-pic"><img src="images/ORVBA.jpeg" alt="' . htmlspecialchars($row['title']) . '"/></div>'; // Project image
-                    echo '<div class="project-content">';
-                        echo '<h1>' . htmlspecialchars($row['title']) . '</h1>'; // Project title
-                        echo '<p>' . htmlspecialchars($row['description']) . '</p>'; // Project description
-                        echo '<a href="' . htmlspecialchars($row['github_url']) . '" target="_blank">Github Link</a>'; // Github link
+                    echo '<div>';
+                        echo '<div class="project-pic"><img src="images/ORVBA.jpeg" alt="' . htmlspecialchars($row['title']) . '"/></div>'; // Project image
+                        echo '<div class="project-content">';
+                            echo '<h3>' . htmlspecialchars($row['title']) . '</h3>'; // Project title
+                            echo '<p>' . htmlspecialchars($row['description']) . '</p>'; // Project description
+                            echo '<a href="' . htmlspecialchars($row['github_url']) . '" target="_blank">Github Link</a>'; // Github link
+                        echo '</div>';
                     echo '</div>'; // Close project-content
                 echo '</div>'; // Close project
             }
